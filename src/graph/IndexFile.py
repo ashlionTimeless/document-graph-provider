99a645b9a4024dceaa3e89c2fe707cadba5be404
MaxLN <max@legalnodes.com>
2025-07-07 14:52:57 +0200

    updated

--- src/graph/IndexFile.py
+++ src/graph/IndexFile.py
@@ -2,27 +2,24 @@ import os
 import subprocess
 MS_GRAPHRAG_ROOT = os.environ.get("MS_GRAPHRAG_ROOT")
 
-cmd_template = [
-        "python3", "-m", "graphrag index",
-        "--root", "./resume-matching",
-        "--method", "local",
-]
+#cmd_template = [
+#        "python3", "-m", "graphrag", "index",
+#        "--config", f"{MS_GRAPHRAG_ROOT}/settings.yaml",
+#        "--root", f"{MS_GRAPHRAG_ROOT}/{folder}/",
+#        "--output", f"{MS_GRAPHRAG_ROOT}/{folder}/output"]
 
 class IndexFile:
     
     def run_index(self,folder):
         cmd = [
-        "python3", "-m", "graphrag", "index",
-        "--config", f"{MS_GRAPHRAG_ROOT}/settings.yaml",
-        "--root", f"{MS_GRAPHRAG_ROOT}/{folder}/",
-        "--output", f"{MS_GRAPHRAG_ROOT}/{folder}/output"]
-
-        cmd = [
             "python3", "/var/www/html/ragtest/script.py"
         ]
         print(cmd)
         try:
+            print("before")
             result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+            print("after")
+            print(result)
             output = result.stdout
             print(output)
             return output
